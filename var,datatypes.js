4c9c5ffb5bd816705b4454b3428481a4165eac21
Your Name <you@example.com>
2024-09-18 23:34:19 +0530

    new files are added

---
+++ var,datatypes.js
@@ -0,0 +1,33 @@
+var mother={
+   name:"Renuka" ,
+   age:30,
+   occupation:"housewife",
+   child:{
+    name:"swetha",
+    age:21,
+    occupation:"student"
+   }
+}
+console.log(mother)
+//array
+var phones=[phones=["samsung","gold", 30000],["redmi","white", 28000],["vivo","blue",28000]]
+console.log(phones);
+//boolean
+var boolean={
+    age:16,
+}
+if(boolean.age=16){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+var boolean={
+    salary:20000,
+}
+if(boolean.salary=16000){
+    console.log(false);
+}
+else{
+    console.log(true);
+}
